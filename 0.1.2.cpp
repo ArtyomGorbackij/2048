1c0a97c828f693ef426d64f3fa18b9b1ac7ebbfb
ArtyomGorbackij <31548827+ArtyomGorbackij@users.noreply.github.com>
2017-12-20 21:44:11 +0300

    Update 0.1.2.cpp

--- 0.1.2.cpp
+++ 0.1.2.cpp
@@ -1,11 +1,10 @@
-#include "stdafx.h"
 #include <iostream>
 #include <sstream>
 using namespace std;
 int main() {
-    int k;
     char q;
-    int m[4][4] = { { 0,4,0,2 },{ 0,0,0,0 },{ 0,2,0,2 },{ 4,0,0,2 } };
+    int p;
+    int m[4][4] = { { 2,2,2,2 },{ 0,2,2,2 },{ 0,2,2,2 },{ 0,0,0,2 } };
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             cout << m[i][j] << " ";
@@ -14,71 +13,121 @@ int main() {
     }
 up:cin >> q;
     switch (q) {
-    case 'l':
-        k = 0;
-        while (k <= 4) {
+        case 'l':
+             p=0;
+                    for(int i=0;i<4;i++){
+                        for(int j=3;j>0;j--){
+                            if (m[i][j]==0)
+                                swap (m[i][j],m[i][j-1]);
+                        }
+                    }
+            for(int i=0;i<4;i++){
+                for(int j=3;j>0;j--){
+                    if(m[i][j]==m[i][j-1]){
+                        int a= m[i][j]+m[i][j-1];
+                        m[i][j]=0;
+                        m[i][j-1]=a;
+                    }
+                }
+            }
+            while ( p<2){
+                    for(int i=0;i<4;i++){
+                        for(int j=3;j>0;j--){
+                            if (m[i][j]==0)
+                                swap (m[i][j],m[i][j-1]);
+                        }
+                    }
+                p++;
+            }
+            break;
+        case 'k':
+             p=0;
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 4; j++) {
+                        if (m[i][j] == 0) {
+                            swap(m[i][j],m[i+1][j]);
+                        }
+                    }
+                }
             for (int i = 0; i < 4; i++) {
                 for (int j = 0; j < 4; j++) {
-                    if (m[i][4 - j] == m[i][3 - j] || m[i][4 - j] == 0) {
-                        if (j != 0) {
-                            m[i][4 - j] = m[i][4 - j] + m[i][3 - j];
-                            m[i][3 - j] = 0;
-                        }
+                    if (m[i][j] == m[i-1][j]) {
+                        int a=m[i][j]+m[i-1][j];
+                        m[i][j]=0;
+                        m[i-1][j]=a;
                     }
                 }
             }
-            k++;
-        }
-        break;
-    case 'k':
-        k = 0;
-        while (k < 4) {
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    if (m[i][j] == m[i + 1][j] || m[i][j] == 0) {
-                        if (i != 3) {
-                            m[i][j] = m[i][j] + m[i + 1][j];
-                            m[i + 1][j] = 0;
-                        }
-                    }
+    while ( p<2){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (m[i][j] == 0) {
+                    swap(m[i][j],m[i+1][j]);
                 }
             }
-            k++;
         }
-        break;
-        case 'h':
-        k = 0;
-        while (k < 4) {
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    if (m[i][j] == m[i][j + 1] || m[i][j] == 0) {
-                        if (j != 3) {
-                            m[i][j] = m[i][j] + m[i][j + 1];
-                            m[i][j + 1] = 0;
-                        }
-                    }
-                }
-            }
-            k++;
-        }
-        break;
-    case'j':
-        k = 0;
-        while (k < 4) {
-            for (int i = 4; i > 0; i--) {
-                for (int j = 0; j < 4; j++) {
-                    if (m[i - 1][j] == m[i][j] || m[i][j] == 0) {
-                        if (i != 0) {
-                            m[i][j] = m[i - 1][j] + m[i][j];
-                            m[i - 1][j] = 0;
-                        }
-                    }
-                }
-            }
-            k++;
-        }
-        break;
+        p++;
     }
+            break;
+        case 'h':
+             p=0;
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 1; j <4; j++) {
+                        if (m[i][j-1] == 0) {
+                            swap(m[i][j],m[i][j-1]);
+                        }
+                    }
+                }
+            for (int i = 0; i < 4; i++) {
+                for (int j = 1; j <4; j++) {
+                        if(m[i][j]==m[i][j-1]){
+                            int a=m[i][j]+m[i][j-1];
+                            m[i][j]=0;
+                            m[i][j-1]=a;
+                        }
+                    }
+                }
+       while ( p<2){
+            for (int i = 0; i < 4; i++) {
+                for (int j = 1; j <4; j++) {
+                    if (m[i][j-1] == 0) {
+                        swap(m[i][j-1],m[i][j]);
+                    }
+                }
+            }
+           p++;
+       }
+            break;
+        case'j':
+            p=0;
+            for (int i = 3; i > 0; i--) {
+                for (int j = 0; j < 4; j++) {
+                    if ( m[i][j] == 0 ) {
+                        swap(m[i][j], m[i - 1][j]);
+                    }
+                }
+            }
+            for (int i = 3; i > 0; i--) {
+                for (int j = 0; j < 4; j++) {
+                    if (m[i][j] == m[i - 1][j]) {
+                        int a = m[i][j] + m[i - 1][j];
+                        m[i][j] = a;
+                        m[i - 1][j] = 0;
+                    }
+                }
+            }
+        while ( p<2){
+            for (int i = 3; i > 0; i--) {
+                for (int j = 0; j < 4; j++) {
+                    if ( m[i][j] == 0 ) {
+                        swap(m[i][j], m[i - 1][j]);
+                    }
+                }
+            }
+            p++;
+        }
+            break;
+            }
     if (q != 'q') {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -88,8 +137,6 @@ up:cin >> q;
         }
         goto up;
     }
-    else return 0;
-    cin.get();
-    cin.get();
+    else cin.get();
     return 0;
 }
